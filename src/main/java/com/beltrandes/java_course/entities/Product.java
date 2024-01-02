dc63b482007088bc51d808f6ff50a357fca82e30
Beltrandes <beltsmarques@hotmail.com>
2024-01-02 15:23:12 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/beltrandes/java_course/entities/Product.java
+++ src/main/java/com/beltrandes/java_course/entities/Product.java
@@ -1,5 +1,6 @@
 package com.beltrandes.java_course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -7,7 +8,6 @@ import java.util.HashSet;
 import java.util.Set;
 
 @Entity
-@Data
 @Table(name = "tb_product")
 @NoArgsConstructor
 @EqualsAndHashCode(of = "id")
@@ -15,16 +15,38 @@ public class Product {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @Getter
+    @Setter
     private Long id;
+    @Getter
+    @Setter
     private String name;
+    @Getter
+    @Setter
     private String description;
+    @Getter
+    @Setter
     private Double price;
+    @Getter
+    @Setter
     private String imgUrl;
 
     @ManyToMany
     @JoinTable(name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
-    @Setter(AccessLevel.NONE)
+    @Getter
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
+
+    @JsonIgnore
+    public Set<Order> getOrders() {
+        Set<Order> set = new HashSet<>();
+        for (OrderItem x : items) {
+            set.add(x.getOrder());
+        }
+        return set;
+    }
 
     public Product(Long id, String name, String description, Double price, String imgUrl) {
         this.id = id;
