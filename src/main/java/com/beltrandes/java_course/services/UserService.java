9e7948340b1efcdc3aa8f274130b140827370b47
Beltrandes Marques <beltsmarques@hotmail.com>
2024-01-02 01:03:52 -0300

    Service layer, component registration

---
+++ src/main/java/com/beltrandes/java_course/services/UserService.java
@@ -0,0 +1,25 @@
+package com.beltrandes.java_course.services;
+
+import com.beltrandes.java_course.entities.User;
+import com.beltrandes.java_course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository repository;
+
+    public List<User> findAll() {
+        return repository.findAll();
+    }
+
+    public User findById(Long id) {
+        Optional<User> obj =  repository.findById(id);
+
+        return obj.get();
+    }
+}
